ec0231be56fde5d89f40dded3ad4e489418d1d4f
Daniel Wayota <danielglez1990@gmail.com>
2020-11-19 16:19:51 +0000

    Tilemap 2D generation

---
+++ Assets/MapGeneration2D/Scripts/MapMaker.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapMaker : MonoBehaviour
+{
+    public Tile tile;
+    public Tilemap tilemap;
+
+    public int mapWidth;
+    public int mapHeight;
+
+    private int[,] mapData;
+
+    public CellularData cell;
+    public PerlinData perlin;
+
+    /// =============================================
+    /// <summary>
+    ///
+    /// </summary>
+    void Start()
+    {
+        this.mapData = this.perlin.GenerateData(this.mapWidth, this.mapHeight);
+
+        this.GenerateTiles();
+    }
+
+    void GenerateTiles()
+    {
+        for (int i = 0; i < this.mapWidth; i++)
+        {
+            for (int j = 0; j < this.mapHeight; j++)
+            {
+                if (this.mapData[i, j] == 1)
+                {
+                    this.tilemap.SetTile(
+                        new Vector3Int(i, j, 0),
+                        this.tile
+                    );
+                }
+            }
+        }
+    }
+
+    /// =============================================
+    /// <summary>
+    ///
+    /// </summary>
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/MapGeneration2D/Scripts/PerlinData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinData : MonoBehaviour
+{
+    public float fillPercent = 0.15f;
+
+    public float scale = 20;
+
+    public int[,] GenerateData(int w, int h)
+    {
+        int[,] mapData = new int[w, h];
+
+        for (int i = 0; i < w; i++)
+        {
+            for (int j = 0; j < h; j++)
+            {
+                float heightPercent = Mathf.Pow(1 - (float)j / (float)h, 3);
+
+                float value = Mathf.PerlinNoise(
+                    (float)i / scale,
+                    (float)j / scale
+                ) - heightPercent;
+
+                mapData[i, j] = value < this.fillPercent ? 1 : 0;
+            }
+        }
+
+        return mapData;
+    }
+}
